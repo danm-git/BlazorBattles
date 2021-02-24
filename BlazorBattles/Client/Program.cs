25d25668c723ab9387874308d4498d37da99ea5c
danm-git <daniel.e.munro@gmail.com>
2021-02-24 16:12:23 -0700

    Hello World danm-git!

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using BlazorBattles.Client.Services;
+using Blazored.Toast;
 
 namespace BlazorBattles.Client
 {
@@ -17,7 +19,10 @@ namespace BlazorBattles.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Services.AddBlazoredToast();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IBananaService, BananaService>();
+            builder.Services.AddScoped<IUnitService, UnitService>();
 
             await builder.Build().RunAsync();
         }
